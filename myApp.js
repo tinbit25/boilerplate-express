cfa290906530c49eaa7c558bcb50b4f15efe25dd
tinbit25 <tinbitelias25@gmail.com>
2024-09-19 00:54:34 +0300

    complete 6 tasks

--- myApp.js
+++ myApp.js
@@ -1,39 +1,27 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
 
+console.log("Hello World")
+// app.get("/",function(req,res){
+//     res.send("Hello Express")
+// })
+app.use("/public",express.static(__dirname+"/public"))   
 
+app.get("/",function(req,res){
+    absolutePath=__dirname+"/views/index.html"
+    res.sendFile(absolutePath)
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+app.get("/json",function(req,res){
+    const messagestyle=process.env.MESSAGE_STYLE 
+    const message="Hello json"
+    if(messagestyle=="uppercase"){
+        res.json({"message": message.toUpperCase()})
+    }
+    else{
+        res.json({"message":message})
+    }
+    
+})
+module.exports = app
